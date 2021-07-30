250bac1a9953238af953d122d2b2a635bd9ac151
serega.starshov <serega.starshov@mail.ru>
2021-07-30 13:39:34 +0300

    основная чать дз

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 let money, income, addExpenses, deposit, mission, period;
+let budgetDay;
 
-alert('Home work');
-console.log('Hello my friend');
+// alert('Home work');
+// console.log('Hello my friend');
+
+money = 25000;
+income = 'Фриланс';
+addExpenses = 'интернет, коммуналка, проезд, продукты';
+deposit = false;
+mission = 5e6;
+period = 7;
+budgetDay = Math.round(money / 30);
+
+console.log(money);
+console.log(income);
+console.log(deposit);
+console.log(addExpenses.length);
+console.log(`Период равен ${period} месяцев и Цель заработать ${mission} рублей`);
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
